1116f64ee779fe61ea379e1e75e3ad5c88a49a7a
leewhittaker-peakai <lee.whittaker@peak.ai>
2023-03-30 16:24:26 +0100

    added more analysis

--- dynamic_containment/utils.py
+++ dynamic_containment/utils.py
@@ -10,6 +10,12 @@ import warnings
 from scipy.interpolate import interp1d
 warnings.filterwarnings('ignore')
 from tqdm.notebook import tqdm
+from scipy.special import erf
+
+
+def probability_tails(x1, x2):
+
+    return 0.5 * erf(x1/np.sqrt(2)) + 0.5 * erf(x2/np.sqrt(2))
 
 
 def hist_fit_2gauss(x, bin_cents, hist):
@@ -36,7 +42,14 @@ def extract_time_block(min_t, max_t, table):
     return table.query("date>=@min_t & date<=@max_t")
 
 
-def battery_power(delta_freq, service_power, max_discharge_rate=5, max_charge_rate=4, charge_efficiency=0.9, max_cap=4):
+def battery_power(
+        delta_freq,
+        service_power,
+        max_discharge_rate=5,
+        max_charge_rate=4,
+        charge_efficiency=0.9,
+        max_cap=4
+):
     
     max_percent = max_charge_rate / service_power
     min_percent = -max_discharge_rate / service_power
@@ -73,13 +86,34 @@ def battery_power(delta_freq, service_power, max_discharge_rate=5, max_charge_ra
     return battery_charge
 
 
-def add_charge_info(input_df, service_power, delta_time=1):
+def add_charge_info(
+        input_df,
+        service_power,
+        delta_time=1,
+        max_discharge_rate=5,
+        max_charge_rate=4,
+        charge_efficiency=0.9,
+        max_cap=4
+):
 
     df = input_df.copy()
     
-    df["battery_charge_percent"] = [battery_power(dfi, service_power) for dfi in df["delta_freq"]]
+    df["battery_charge_percent"] = [
+        battery_power(
+            dfi,
+            service_power,
+            max_discharge_rate,
+            max_charge_rate,
+            charge_efficiency,
+            max_cap)
+        for dfi in df["delta_freq"]
+    ]
     df["battery_charge"] = service_power * df["battery_charge_percent"]
-    df['culm_charge'] = df["battery_charge"].cumsum() * delta_time/3600.0
+    df["battery_charge_low"] = np.where(df["delta_freq"] <= 0.015, service_power * df["battery_charge_percent"], 0.0)
+    df["battery_charge_high"] = np.where(df["delta_freq"] >= 0.015, service_power * df["battery_charge_percent"], 0.0)
+    df['culm_charge'] = df["battery_charge"].cumsum() * delta_time/3600.0 
+    df['culm_charge_low'] = df["battery_charge_low"].cumsum() * delta_time/3600.0
+    df['culm_charge_high'] = df["battery_charge_high"].cumsum() * delta_time/3600.0
     
     return df
 
@@ -158,10 +192,27 @@ def normal_dist(x, sigma, mu=0):
     return np.exp(-0.5 * (np.abs(x-mu)**2.0 / sigma**2.0)) / (np.sqrt(2.0 * np.pi) * sigma)
 
 
-def calc_gaussian_slope(sigma, service_power):
+def calc_gaussian_slope(
+        sigma,
+        service_power,
+        max_discharge_rate=5,
+        max_charge_rate=4,
+        charge_efficiency=0.9,
+        max_cap=4
+):
     
     delta_freq = np.linspace(-1.5, 1.5, num=200)
-    y = np.array([battery_power(deltaf, service_power) for deltaf in delta_freq])
+    y = np.array(
+        [
+            battery_power(
+                deltaf,
+                service_power,
+                max_discharge_rate,
+                max_charge_rate,
+                charge_efficiency,
+                max_cap
+
+            ) for deltaf in delta_freq])
     
     charge = np.trapz(
         (
@@ -173,10 +224,29 @@ def calc_gaussian_slope(sigma, service_power):
     return charge * service_power / 3600
 
 
-def calc_fitted_slope(pdf, bin_cents, service_power):
+def calc_fitted_slope(
+        pdf,
+        bin_cents,
+        service_power,
+        max_discharge_rate=5,
+        max_charge_rate=4,
+        charge_efficiency=0.9,
+        max_cap=4
+):
     
     delta_freq = bin_cents#np.linspace(-1.5, 1.5, num=2000)
-    y = np.array([battery_power(deltaf, service_power) for deltaf in delta_freq])
+    y = np.array(
+        [
+            battery_power(
+                deltaf,
+                service_power,
+                max_discharge_rate,
+                max_charge_rate,
+                charge_efficiency,
+                max_cap
+            ) for deltaf in delta_freq
+        ]
+    )
     
     interped_pdf = interp1d(bin_cents, pdf, fill_value='extrapolate',  kind='linear')
     
@@ -190,7 +260,15 @@ def calc_fitted_slope(pdf, bin_cents, service_power):
     return charge * service_power / 3600
 
 
-def calc_data_slope(input_df, service_power, bins=200):
+def calc_data_slope(
+        input_df,
+        service_power,
+        bins=200,
+        max_discharge_rate=5,
+        max_charge_rate=4,
+        charge_efficiency=0.9,
+        max_cap=4
+):
 
     df = input_df.copy()
     
@@ -198,7 +276,18 @@ def calc_data_slope(input_df, service_power, bins=200):
     
     delta_freq = 0.5 * (x[1:] + x[:-1])
     
-    y = np.array([battery_power(deltaf, service_power) for deltaf in delta_freq])
+    y = np.array(
+        [
+            battery_power(
+                deltaf,
+                service_power,
+                max_discharge_rate,
+                max_charge_rate,
+                charge_efficiency,
+                max_cap
+            ) for deltaf in delta_freq
+        ]
+    )
     
     charge = np.trapz(
         (
@@ -285,10 +374,7 @@ def square_cov(cov, max_time, time_step=1):
 
 def simulate_grf_using_cov(cov, max_time, time_step, num_samples, means=0):
     
-    print("creating covariance")
     cov_arr = square_cov(cov, max_time, time_step=time_step)
-    
-    print("creating samples")
     samples = np.random.multivariate_normal([means]*len(cov_arr[0]), cov_arr, size=num_samples)
     
     return samples
@@ -325,3 +411,195 @@ def create_continuous_timeseries(input_df):
     return df
 
 
+def simulations_for_anaylsis(
+        real_space_cov,
+        service_powers,
+        nreals,
+        time_step,
+        num_days,
+        block_length,
+        max_discharge_rate=5,
+        max_charge_rate=4,
+        charge_efficiency=0.9,
+        max_cap=4
+):
+
+    num_blocks = np.ceil(num_days * 24 * 3600 / float(block_length))
+
+    reals = {}
+
+    summaries = {}
+
+    num_samples = [nreals, int(num_blocks)]
+
+    for p in service_powers:
+
+        print(f"creating simulation for service power {p}")
+    
+        samples = simulate_grf_using_cov(real_space_cov, max_time=10800, time_step=time_step, num_samples=num_samples)
+    
+        new_samples = [s.flatten() for s in samples]
+    
+        #new_samples = utils.interp_samples(samples, max_time=num_seconds, step_samples=time_step)
+    
+        count = 0
+    
+        reals[p] = {}
+
+        for s in new_samples:
+        
+            df = pd.DataFrame()
+            df["date"] = np.arange(len(s))*time_step
+            df["delta_freq"] = s
+            df = add_charge_info(
+                df,
+                p,
+                time_step,
+                max_discharge_rate,
+                max_charge_rate,
+                charge_efficiency,
+                max_cap
+            )
+
+            reals[p][count] = df
+        
+            count+=1
+        
+        summaries[p] = reals[p][0].copy()
+        summaries[p]['culm_charge'] = 0.0
+        summaries[p]['culm_charge_sq'] = 0.0
+
+        for s in reals[p]:
+        
+            summaries[p]['culm_charge'] += reals[p][s]['culm_charge']
+            summaries[p]['culm_charge_sq'] += reals[p][s]['culm_charge']**2.0
+
+        summaries[p]['culm_charge'] = summaries[p]['culm_charge'] / len(reals[p])
+    
+        summaries[p]['var_charge'] = (
+            summaries[p]['culm_charge_sq'] / (len(reals[p]) - 1)
+            - summaries[p]['culm_charge']**2.0 * len(reals[p]) / (len(reals[p]) - 1)
+        )
+    
+        summaries[p]['std_charge'] = np.sqrt(summaries[p]['var_charge'])
+
+    return summaries, reals
+
+
+def max_date_func(E0, reals, p, max_cap, service='both'):
+    
+    date_end = []
+
+    if service != 'both':
+        tag = '_' + service
+    else:
+        tag = ''
+
+    for s in reals[p]:
+
+        charge = E0 + np.array(reals[p][s][f"culm_charge{tag}"])
+
+        min_date = np.where(charge < 0.0)[0]
+        if len(min_date):
+            min_date = np.min(min_date)
+        else:
+            min_date = 1e10
+        max_date = np.where(charge > max_cap)[0]
+        if len(max_date):
+            max_date = np.min(max_date)
+        else:
+            max_date = 1e10
+        date_end.append(np.min([min_date, max_date]))
+        
+    return np.array(date_end)
+
+
+def find_E0_for_max_time(
+        reals,
+        p,
+        max_cap,
+        min_charge_lim,
+        max_charge_lim,
+        num_charge_steps,
+        poly_deg
+):
+
+    E_arr = np.linspace(min_charge_lim, max_charge_lim, num=num_charge_steps)
+
+    d_mean = []
+    
+    for e in E_arr:
+
+        dates = max_date_func(e, reals, p, max_cap)
+
+        d_mean.append(np.mean(dates))
+
+    res = np.polyfit(E_arr, d_mean, deg=poly_deg)
+
+    Ebest = E_arr[np.where(np.poly1d(res)(E_arr)==np.max(np.poly1d(res)(E_arr)))[0]]
+
+    return Ebest, d_mean, E_arr
+
+
+def service_power_loop(
+        reals,
+        service_powers,
+        max_cap,
+        time_step,
+        min_charge_lim,
+        max_charge_lim,
+        num_charge_steps,
+        service='both',
+        poly_deg=8,
+        fit_conf=0.95,
+        verbose=True
+):
+
+    results = {}
+
+    for p in service_powers:
+
+        results[p] = {}
+
+        if service == 'both':
+            
+
+            E_best, d_mean, E_arr = find_E0_for_max_time(
+                reals,
+                p,
+                max_cap,
+                min_charge_lim,
+                max_charge_lim,
+                num_charge_steps,
+                poly_deg
+            )
+            
+        elif service=='high':
+            
+            E_best = [0]
+            
+        elif service=='low':
+            
+            E_best = [max_cap]
+            
+        dates = max_date_func(E_best, reals, p, max_cap, service)*time_step /(3600.0 * 24.0)
+
+        dates_sort = np.sort(dates)
+
+        sig_lower = dates_sort[int(len(dates) * (1-fit_conf)/2.0)]
+        sig_upper = dates_sort[int(len(dates) * (1 - (1-fit_conf)/2.0))]
+
+        results[p]["E_best"] = E_best
+        results[p]["exp_date"] = np.mean(dates)
+        results[p]["date_lower"] = sig_lower
+        results[p]["date_higher"] = sig_upper
+        results[p]["dates"] = dates
+
+        if verbose:
+        
+            print(f"For service power {p} MW")
+            print(f"\tThe expected date of termination is {np.round(np.mean(dates), 2)} days")
+            print(f"\tThe {100*fit_conf}% confidence interval is {np.round(sig_lower, 2)} - {np.round(sig_upper, 2)} days")
+            print(f"\tWith initial charge = {E_best[0]} MWh")
+
+    return results
